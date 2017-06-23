5702822fada2d68bdd7e53a08a07eec08fb46cd7
邝业亨 <kuangyeheng1@yy.com>
2017-06-23 17:31:24 +0800

    inp.js done

--- bin/inp.js
+++ bin/inp.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
 
 'use strict';
+var pkg = require('../package.json');
+var program = require('commander');
+var args = process.argv.slice(2);
+var path;
 
-require('../index')();
+program
+    .version(pkg.version)
+    .usage('[options] <path>')
+    .option('-l, --local', 'Copy from local')
+    .parse(process.argv);
+
+if (program.args.length !== 1) {
+    program.help();
+}
+
+if (args.length > 1) {
+    program.help();
+}
+
+path = program.args[0];
+
+require('../index')(path,program.local);
