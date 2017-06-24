38e3ea3e560c3e5bb4f3897b076b57ca985b1af5
邝业亨 <kuangyeheng1@yy.com>
2017-06-24 16:12:06 +0800

    ci index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
-module.exports = function (projectName, projectSeedPath, isFromLocal) {
+var chalk = require('chalk');
 
+module.exports = function (projectName, projectSeedPath, isFromLocal) {
+    /* eslint-disable no-console */
+    console.log(chalk.green('projectName')+':',chalk.green.bold(projectName)+'\n');
+    console.log(chalk.yellow('projectSeedPath')+':',chalk.yellow.bold(projectSeedPath)+'\n');
+    console.log(chalk.magenta('isFromLocal')+':',chalk.magenta.bold(!!isFromLocal)+'\n');
+    /* eslint-enable no-console */
+    
 };
