9334decab319cca8400906ab6741e14872cc804a
邝业亨 <kuangyeheng1@yy.com>
2017-06-24 14:57:12 +0800

    ci index.js

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 'use strict';
 
-var inquirer = require('inquirer');
-var chalk = require('chalk');
-var downloadGitRepo = require('download-git-repo');
+module.exports = function (projectName, projectSeedPath, isFromLocal) {
 
-var createQuestion = function  (keyword) {
-	return [
-        {
-            type: 'list',
-            name: 'type',
-            message: chalk.yellow.blod(keyword) + 'is?',
-            choices: [
-                'projectName',
-                'projectSeedPath'
-            ]
-        }
-    ]
-};
-
-module.exports = function  (projectName, projectSeedPath, isFromLocal) {
-    var keyword;
-    if (!(projectName && projectSeedPath)) {
-        keyword = projectName || projectSeedPath;
-        if (keyword) {
-            console.log(keyword);
-        }else{
-            console.log(keyword);
-        }
-    }
 };
