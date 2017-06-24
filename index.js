744b34c240221e0833bad5c109af342dcfd1a0bd
邝业亨 <kuangyeheng1@yy.com>
2017-06-24 13:19:27 +0800

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+'use strict';
 
-module.exports = function  () {
-	console.log(11111111111);
+var inquirer = require('inquirer');
+var chalk = require('chalk');
+var downloadGitRepo = require('download-git-repo');
+
+var createQuestion = function  (keyword) {
+	return [
+        {
+            type: 'list',
+            name: 'type',
+            message: chalk.yellow.blod(keyword) + 'is?',
+            choices: [
+                'projectName',
+                'projectSeedPath'
+            ]
+        }
+    ]
+};
+
+module.exports = function  (projectName, projectSeedPath, isFromLocal) {
+    var keyword;
+    if (!(projectName && projectSeedPath)) {
+        keyword = projectName || projectSeedPath;
+        if (keyword) {
+            console.log(keyword);
+        }else{
+            console.log(keyword);
+        }
+    }
 };
